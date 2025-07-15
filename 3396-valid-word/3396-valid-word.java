60c885f8ab0e886be7d4309ee67a330c41fd3c4b
Anshu Bagne <120034008+Anshu-Bagne@users.noreply.github.com>
2025-07-15 13:24:03 +0530

    Time: 1 ms (99.12%), Space: 42 MB (80.13%) - LeetHub

---
+++ 3396-valid-word/3396-valid-word.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValid(String word) {
+        if (word.length() < 3) return false;
+
+        int countVowels = 0;
+        int countConsonants = 0;
+
+        for (char ch : word.toCharArray()) {
+            if (!Character.isLetterOrDigit(ch)) return false;
+
+            if (Character.isLetter(ch)) {
+                char lower = Character.toLowerCase(ch);
+                if (isVowel(lower)) {
+                    countVowels++;
+                } else {
+                    countConsonants++;
+                }
+            }
+        }
+
+        if (countVowels == 0 || countConsonants == 0) return false;
+
+        return true;
+    }
+
+    private boolean isVowel(char ch) {
+        return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
+    }
+}
